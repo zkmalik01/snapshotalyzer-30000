2e36b2f30d5ce9b11ca79bd2bb0d77fcd6b2d410
Amtul Malik <75287754+zkmalik01@users.noreply.github.com>
2021-02-11 15:47:45 -0500

    update

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,7 +1,7 @@
 import boto3
 
-if __name__ = '__main__':
-    session = boto3.Session(profile_name='shotty')
+if __name__ == '__main__':
+    session = boto3.session(profile_name='shotty')
     ec2 = session.resource('ec2')
 
     for i in ec2.instances.all():
